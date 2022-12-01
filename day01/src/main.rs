d94f24e1ccca41f1b6a2b164a2a58d5403c1f533
Jeremy E Kozdon <jeremy@kozdon.net>
2022-11-30 21:28:23 -0800

    Day01 solution

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,36 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file = &args[1];
+    let file = fs::read_to_string(file).expect("Should have been able to read the file");
+
+    let mut sum = 0;
+    let mut a = 0;
+    let mut b = 0;
+    let mut c = 0;
+
+    for line in file.lines() {
+        if line == "" {
+            if sum >= a {
+                c = b;
+                b = a;
+                a = sum;
+            } else if sum >= b {
+                c = b;
+                b = sum;
+            } else if sum >= c {
+                c = sum;
+            }
+            sum = 0;
+        } else {
+            let val: u32 = line.trim().parse().expect("Wanted a number");
+            sum = sum + val;
+        }
+    }
+
+    println!("maximum value:    {}", a);
+    println!("sum of top three: {}", a + b + c);
 }
