1cee265eec5a50e325e514c7fe5041e7449a98e5
Jeremy E Kozdon <jeremy@kozdon.net>
2022-12-01 22:07:26 -0800

    Another mathch solution

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -16,51 +16,53 @@ fn main() {
         // play:  rock(1), paper(2), scissors(3)
         // op: ABC -> rock, paper, scissors
         // v1: XYZ -> rock, paper, scissors
-        match tok[0] {
-            "A" => match tok[1] {
-                "X" => score1 = score1 + 1 + 3,
-                "Y" => score1 = score1 + 2 + 6,
-                "Z" => score1 = score1 + 3 + 0,
-                _ => (),
-            },
-            "B" => match tok[1] {
-                "X" => score1 = score1 + 1 + 0,
-                "Y" => score1 = score1 + 2 + 3,
-                "Z" => score1 = score1 + 3 + 6,
-                _ => (),
-            },
-            "C" => match tok[1] {
-                "X" => score1 = score1 + 1 + 6,
-                "Y" => score1 = score1 + 2 + 0,
-                "Z" => score1 = score1 + 3 + 3,
-                _ => (),
-            },
-            _ => (),
-        }
+        score1 = score1
+            + match tok[0] {
+                "A" => match tok[1] {
+                    "X" => 1 + 3,
+                    "Y" => 2 + 6,
+                    "Z" => 3 + 0,
+                    _ => panic!(),
+                },
+                "B" => match tok[1] {
+                    "X" => 1 + 0,
+                    "Y" => 2 + 3,
+                    "Z" => 3 + 6,
+                    _ => panic!(),
+                },
+                "C" => match tok[1] {
+                    "X" => 1 + 6,
+                    "Y" => 2 + 0,
+                    "Z" => 3 + 3,
+                    _ => panic!(),
+                },
+                _ => panic!(),
+            };
 
         // op: ABC -> rock, paper, scissors
         // v2: XYZ -> lose, draw, win
-        match tok[0] {
-            "A" => match tok[1] {
-                "X" => score2 = score2 + 3 + 0,
-                "Y" => score2 = score2 + 1 + 3,
-                "Z" => score2 = score2 + 2 + 6,
-                _ => (),
-            },
-            "B" => match tok[1] {
-                "X" => score2 = score2 + 1 + 0,
-                "Y" => score2 = score2 + 2 + 3,
-                "Z" => score2 = score2 + 3 + 6,
-                _ => (),
-            },
-            "C" => match tok[1] {
-                "X" => score2 = score2 + 2 + 0,
-                "Y" => score2 = score2 + 3 + 3,
-                "Z" => score2 = score2 + 1 + 6,
-                _ => (),
-            },
-            _ => (),
-        }
+        score2 = score2
+            + match tok[0] {
+                "A" => match tok[1] {
+                    "X" => 3 + 0,
+                    "Y" => 1 + 3,
+                    "Z" => 2 + 6,
+                    _ => panic!(),
+                },
+                "B" => match tok[1] {
+                    "X" => 1 + 0,
+                    "Y" => 2 + 3,
+                    "Z" => 3 + 6,
+                    _ => panic!(),
+                },
+                "C" => match tok[1] {
+                    "X" => 2 + 0,
+                    "Y" => 3 + 3,
+                    "Z" => 1 + 6,
+                    _ => panic!(),
+                },
+                _ => panic!(),
+            };
     }
 
     println!("score1: {}", score1);
