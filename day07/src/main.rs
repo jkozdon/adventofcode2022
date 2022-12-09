de45ebbd28131d996790b8c17ecb587ba2f6342c
Jeremy E Kozdon <jeremy@kozdon.net>
2022-12-08 19:34:07 -0800

    day06 working solution (not so elegant!)
    Forgot that directories can have same name!

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -27,7 +27,6 @@ impl Directory {
         match self.files.entry(name.clone()) {
             Entry::Vacant(e) => {
                 e.insert(size);
-                self.size = self.size + size;
                 size
             }
             _ => 0,
@@ -46,10 +45,10 @@ fn main() {
     let file = fs::read_to_string(file).expect("Should have been able to read the file");
 
     let mut dirs = HashMap::new();
-    let mut stack = vec!["/"];
-    dirs.insert("/", Directory::new());
+    let mut stack = vec!["/".to_string()];
+    dirs.insert("/".to_string(), Directory::new());
 
-    for line in file.lines() {
+    for line in file.lines().skip(1) {
         if line.chars().nth(0).unwrap() == '$' {
             match line {
                 "$ ls" => {}
@@ -57,15 +56,20 @@ fn main() {
                     stack.pop();
                 }
                 line => {
-                    stack.push(line.splitn(3, ' ').nth(2).unwrap());
+                    let mut newdir = stack.last().unwrap().clone();
+                    newdir.push_str(line.splitn(3, ' ').nth(2).unwrap());
+                    newdir.push_str("/");
+                    stack.push(newdir);
                 }
             };
         } else if line.chars().nth(0).unwrap() == 'd' {
-            let dir = line.splitn(2, ' ').nth(1).unwrap();
-            dirs.insert(dir, Directory::new());
+            let mut newdir = stack.last().unwrap().clone();
+            newdir.push_str(line.splitn(2, ' ').nth(1).unwrap());
+            newdir.push_str("/");
+            dirs.insert(newdir.clone(), Directory::new());
             dirs.get_mut(stack.last().unwrap())
                 .unwrap()
-                .add_subdir(dir.to_string());
+                .add_subdir(newdir);
         } else {
             let (size, name) = line.split_once(' ').unwrap();
             let size: u64 = size.parse().expect("number");
@@ -82,10 +86,16 @@ fn main() {
     }
 
     let mut sum: u64 = 0;
+    let mut delsize = dirs.get_mut(&"/".to_string()).unwrap().size;
+    let required = delsize - 40000000;
     for dir in dirs.values() {
         if dir.size <= 100000 {
             sum = sum + dir.size;
         }
+        if dir.size >= required && dir.size < delsize {
+            delsize = dir.size;
+        }
     }
-    println!("{}", sum);
+    println!("sum of dir <= 100000: {}", sum);
+    println!("delete dir size:      {}", delsize);
 }
