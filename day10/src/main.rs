47e5945ff6d0fef1740d44dc1443d2a9ed647def
Jeremy E Kozdon <jeremy@kozdon.net>
2022-12-11 14:30:04 -0800

    day10 solution

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,3 +1,64 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file = &args[1];
+    let file = fs::read_to_string(file).expect("Should have been able to read the file");
+
+    let mut cycle: i32 = 1;
+    let mut total: i32 = 1;
+    let mut result: i32 = 0;
+    let mut check: i32 = 20;
+    for line in file.lines() {
+        cycle += 1;
+        if cycle == check {
+            result += cycle * total;
+        }
+        if line.len() > 4 {
+            let num: i32 = line.split_once(' ').unwrap().1.parse().unwrap();
+            total += num;
+            cycle += 1;
+            if cycle == check {
+                result += cycle * total;
+            }
+        }
+        if cycle >= check {
+            check += 40;
+        }
+        if cycle >= 220 {
+            break;
+        }
+    }
+    println!("strength sum: {}", result);
+
+    let mut pixel: i32 = 0;
+    let mut X: i32 = 1;
+    for line in file.lines() {
+        if X - 1 == pixel || X == pixel || X + 1 == pixel {
+            print!("#");
+        } else {
+            print!(" ");
+        }
+        pixel += 1;
+        if pixel % 40 == 0 {
+            println!("");
+            pixel = 0;
+        }
+        if line.len() > 4 {
+            let num: i32 = line.split_once(' ').unwrap().1.parse().unwrap();
+            if X - 1 == pixel || X == pixel || X + 1 == pixel {
+                print!("#");
+            } else {
+                print!(" ");
+            }
+            pixel += 1;
+            if pixel % 40 == 0 {
+                println!("");
+                pixel = 0;
+            }
+            X += num;
+        }
+    }
 }
