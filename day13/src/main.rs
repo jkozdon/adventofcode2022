eb5c17f9d896007fe25c4b943ecd36874ec18a42
Jeremy E Kozdon <jeremy@kozdon.net>
2022-12-14 18:35:40 -0800

    day13 work

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+use std::fs;
+fn verify(a: &[u8], b: &[u8]) -> bool {
+    let (mut i, mut j) = (0, 0);
+    if a[i] == '[' as u8 && b[j] == '[' as u8 {
+    } else if a[i] == '[' as u8 {
+    } else if b[j] == '[' as u8 {
+    } else {
+    }
+    true
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file = &args[1];
+    let file = fs::read_to_string(file).expect("Should have been able to read the file");
+
+    for l in file.split("\n\n") {
+       let (a, b) = l.split_once("\n").unwrap();
+       println!("{}", a);
+       println!("{}", b);
+       println!("{}", verify(a.as_bytes(), b.as_bytes()));
+       println!("----");
+    }
 }
