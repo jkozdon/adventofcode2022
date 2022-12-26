70d1662796d65bf50645b2831de137b129bfa766
Jeremy E Kozdon <jeremy@kozdon.net>
2022-12-25 16:05:35 -0800

    day17 part a

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,3 +1,138 @@
+use std::env;
+use std::fs;
+
+fn show_chamber(chamber: &Vec<u8>, top: usize) {
+    for i in (0..top + 1).rev() {
+        print!("|");
+        for j in (0..7).rev() {
+            if chamber[i] & (1 << j) == 0 {
+                print!(".");
+            } else {
+                print!("#");
+            }
+        }
+        println!("|");
+    }
+}
+
+fn valid_move(shape: &Vec<u8>, chamber: &Vec<u8>, bottom: usize) -> bool {
+    let mut valid = true;
+    for (i, block) in (*shape).iter().enumerate() {
+        valid = valid && (*block & chamber[bottom + i] == 0);
+    }
+    valid
+}
+
+fn get_shape(num: u32) -> (Vec<u8>, (u8, u8)) {
+    let num = num % 5;
+    if num == 0 {
+        (
+            vec![(1 << 4) | (1 << 3) | (1 << 2) | (1 << 1)], //
+            (1, 4),
+        )
+    } else if num == 1 {
+        (
+            vec![
+                1 << 3,                         //
+                (1 << 4) | (1 << 3) | (1 << 2), //
+                1 << 3,
+            ], //
+            (2, 4),
+        )
+    } else if num == 2 {
+        (
+            vec![
+                (1 << 4) | (1 << 3) | (1 << 2), //
+                (1 << 2),                       //
+                (1 << 2),
+            ], //
+            (2, 4),
+        )
+    } else if num == 3 {
+        (
+            vec![
+                (1 << 4), //
+                (1 << 4), //
+                (1 << 4), //
+                (1 << 4),
+            ], //
+            (4, 4),
+        )
+    } else if num == 4 {
+        (
+            vec![
+                (1 << 4) | (1 << 3), //
+                (1 << 4) | (1 << 3),
+            ], //
+            (3, 4),
+        )
+    } else {
+        panic!();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file = &args[1];
+    let file = fs::read_to_string(file).expect("Should have been able to read the file");
+
+    let mut chamber = vec![0 as u8; 2022 * 4];
+    chamber[0] = !0;
+    let mut stack_top = 0 as usize;
+
+    let mut shape_num = 0 as u32;
+    let wind: Vec<char> = file.trim().chars().collect();
+    let mut wind_num = 0 as usize;
+
+    loop {
+        let (mut shape, mut sides) = get_shape(shape_num);
+        shape_num += 1;
+        let mut bottom = stack_top + 4;
+        loop {
+            if wind[wind_num] == '<' {
+                if sides.1 < 6 {
+                    for block in shape.iter_mut() {
+                        *block <<= 1;
+                    }
+                    if valid_move(&shape, &chamber, bottom) {
+                        sides = (sides.0 + 1, sides.1 + 1);
+                    } else {
+                        for block in shape.iter_mut() {
+                            *block >>= 1;
+                        }
+                    }
+                }
+            } else {
+                if sides.0 > 0 {
+                    for block in shape.iter_mut() {
+                        *block >>= 1;
+                    }
+                    if valid_move(&shape, &chamber, bottom) {
+                        sides = (sides.0 - 1, sides.1 - 1);
+                    } else {
+                        for block in shape.iter_mut() {
+                            *block <<= 1;
+                        }
+                    }
+                }
+            }
+            wind_num = (wind_num + 1) % wind.len();
+
+            if valid_move(&shape, &chamber, bottom - 1) {
+                bottom -= 1;
+            } else {
+                break;
+            }
+        }
+        for (i, block) in shape.iter_mut().enumerate() {
+            chamber[bottom + i] |= *block;
+        }
+        stack_top = std::cmp::max(stack_top, bottom + shape.len() - 1);
+        //show_chamber(&chamber, stack_top);
+        if shape_num == 2022 {
+            break;
+        }
+    }
+    println!("{}", stack_top);
 }
