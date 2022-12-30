7cbafcd157ff38b44314d1fb33cfc6f69d05d192
Jeremy E Kozdon <jeremy@kozdon.net>
2022-12-30 12:19:56 -0800

    day20 input parsing

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file = &args[1];
+    let file = fs::read_to_string(file).expect("Should have been able to read the file");
+    let data: Vec<i32> = file
+        .lines()
+        .map(|l| l.trim().parse::<i32>().expect("number"))
+        .collect();
 }
