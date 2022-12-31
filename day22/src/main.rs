3937b8f8762bc17846dc64558f6d4ba0b6c8e96c
Jeremy E Kozdon <jeremy@kozdon.net>
2022-12-30 22:04:38 -0800

    day22: parsing

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file = &args[1];
+    let file = fs::read_to_string(file).expect("Should have been able to read the file");
+
+    let mut map: Vec<Vec<char>> = Vec::new();
+    for line in file.lines() {
+        map.push(line.chars().collect());
+    }
+    let route = map.pop().unwrap();
+    assert!(map.pop().unwrap().len() == 0);
+    let mut pos: (usize, usize) = (0, 0);
+    for (i, v) in map[0].iter().enumerate() {
+        if *v == '.' {
+            pos = (0, i);
+            break;
+        }
+    }
 }
