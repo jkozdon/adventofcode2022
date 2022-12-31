725b116ae92840608662e04811cccdad81573158
Jeremy E Kozdon <jeremy@kozdon.net>
2022-12-30 21:47:55 -0800

    day21 full solution

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -2,6 +2,58 @@ use std::collections::HashMap;
 use std::env;
 use std::fs;
 
+fn has_humn(name: &str, data: &HashMap<&str, &str>) -> bool {
+    if name == "humn" {
+        true
+    } else {
+        let expr = data.get(name).unwrap().trim();
+        match expr.parse::<u64>() {
+            Ok(n) => false,
+            _ => {
+                let tok: Vec<&str> = expr.splitn(3, ' ').collect();
+                has_humn(tok[0], data) || has_humn(tok[2], data)
+            }
+        }
+    }
+}
+
+fn get_humn(cur_res: u64, name: &str, data: &HashMap<&str, &str>) -> u64 {
+    if name == "humn" {
+        return cur_res;
+    }
+    let expr = data.get(name).unwrap().trim();
+    let tok: Vec<&str> = expr.splitn(3, ' ').collect();
+    let a = tok[0];
+    let b = tok[2];
+    let first = has_humn(a, data);
+    let new_res = if first {
+        get_result(b, data)
+    } else {
+        get_result(a, data)
+    };
+    if name == "root" {
+        get_humn(new_res, if first { a } else { b }, data)
+    } else if tok[1] == "+" {
+        get_humn(cur_res - new_res, if first { a } else { b }, data)
+    } else if tok[1] == "-" {
+        if first {
+            get_humn(cur_res + new_res, a, data)
+        } else {
+            get_humn(new_res - cur_res, b, data)
+        }
+    } else if tok[1] == "*" {
+        get_humn(cur_res / new_res, if first { a } else { b }, data)
+    } else if tok[1] == "/" {
+        if first {
+            get_humn(cur_res * new_res, a, data)
+        } else {
+            panic!()
+        }
+    } else {
+        panic!()
+    }
+}
+
 fn get_result(name: &str, data: &HashMap<&str, &str>) -> u64 {
     let expr = data.get(name).unwrap().trim();
     match expr.parse::<u64>() {
@@ -37,4 +89,6 @@ fn main() {
         data.insert(name, expr);
     }
     println!("root: {}", get_result("root", &data));
+
+    println!("humn: {}", get_humn(0, "root", &data));
 }
